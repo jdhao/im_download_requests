e03160c7b1313e15904ecf036eff54cf3f1939fd
jdhao <jdhao@hotmail.com>
2022-01-08 18:24:22 +0800

    refactor: reduce code duplication

--- compare.py
+++ compare.py
@@ -16,6 +16,18 @@ def read_urls(txt_path):
     return urls
 
 
+num_run = 10
+im_num = 20
+
+url_file = "img_links.txt"
+im_urls = read_urls(url_file)
+print(f"URL num: {len(im_urls)}")
+selected_urls = random.choices(im_urls, k=im_num)
+
+sess1 = requests.Session()
+sess2 = requests.Session()
+
+
 def get_img(url, session=None):
     r = session.get(url, timeout=5, stream=True)
     # https://requests.readthedocs.io/en/master/user/quickstart/#raw-response-content
@@ -38,11 +50,6 @@ def get_img4(url):
     return r.content
 
 
-sess1 = requests.Session()
-sess2 = requests.Session()
-get_im_partial = partial(get_img, session=sess1)
-get_im_partial2 = partial(get_img2, session=sess2)
-
 def get_img_multithread(urls, func):
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         # result will be a generator, not plain list.
@@ -50,40 +57,36 @@ def get_img_multithread(urls, func):
     return res
 
 
+def get_running_time(func):
+    start = time.time()
+    get_img_multithread(selected_urls, func)
+
+    return time.time() - start
+
+
+get_im_partial = partial(get_img, session=sess1)
+get_im_partial2 = partial(get_img2, session=sess2)
+
+
 def main():
-    num_run = 20
-    im_num = 20
-
-    url_file = "img_links.txt"
-    im_urls = read_urls(url_file)
-    print(f"URL num: {len(im_urls)}")
-
     total_time1 = 0
     total_time2 = 0
     total_time3 = 0
     total_time4 = 0
+
     for i in range(num_run):
         print(f"run {i+1}/{num_run}")
-        urls = random.choices(im_urls, k=im_num)
 
-        start = time.time()
-        res = get_img_multithread(urls, get_im_partial)
-        elapse = time.time() - start
+        elapse = get_running_time(get_im_partial)
         total_time1 += elapse
 
-        start = time.time()
-        res = get_img_multithread(urls, get_im_partial2)
-        elapse = time.time() - start
+        elapse = get_running_time(get_im_partial2)
         total_time2 += elapse
 
-        start = time.time()
-        res = get_img_multithread(urls, get_img3)
-        elapse = time.time() - start
+        elapse = get_running_time(get_img3)
         total_time3 += elapse
 
-        start = time.time()
-        res = get_img_multithread(urls, get_img4)
-        elapse = time.time() - start
+        elapse = get_running_time(get_img4)
         total_time4 += elapse
 
     print(f"avg time (r.raw with session): {total_time1/num_run}")
